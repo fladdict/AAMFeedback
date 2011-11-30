ecccf729521550ccdd16e2afe2313950fec88eec
fladdict <fukatsu@gmail.com>
2011-12-01 02:12:14 +0900

    minor update
    Signed-off-by: fladdict <fukatsu@gmail.com>

--- AAMFeedback/AAMFeedback/AAMFeedbackViewController.m
+++ AAMFeedback/AAMFeedback/AAMFeedbackViewController.m
@@ -402,6 +402,11 @@
     if ([platform isEqualToString:@"iPod1,1"])   return @"iPod Touch 1G";
     if ([platform isEqualToString:@"iPod2,1"])   return @"iPod Touch 2G";
     if ([platform isEqualToString:@"iPod3,1"])   return @"iPod Touch 3G";
+    if ([platform isEqualToString:@"iPod4,1"])   return @"iPod Touch 4G";
+    if ([platform isEqualToString:@"iPad1,1"])   return @"iPad";
+    if ([platform isEqualToString:@"iPad2,1"])   return @"iPad 2 (WiFi)";
+    if ([platform isEqualToString:@"iPad2,2"])   return @"iPad 2 (GSM)";
+    if ([platform isEqualToString:@"iPad2,3"])   return @"iPad 2 (CDMA)";
     if ([platform isEqualToString:@"i386"])   return @"iPhone Simulator";
     if ([platform isEqualToString:@"x86_64"])   return @"iPhone Simulator";
     return platform;
